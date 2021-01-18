6319089d8a953e6253f01f0050fc953806404110
Abhishek Vaish <abhishek953566936222@gmail.com>
2021-01-18 18:02:04 +0530

    highlighted text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,18 @@ const IndexPage = ({data}) => {
 	const search = (e) => {
 		const searchString = e.target.value.toLowerCase().trim();
 		const filteredData  = data.allMarkdownRemark.edges.filter((edge) => {
-			return edge.node.frontmatter.title.toLowerCase().includes(searchString) ||
-			edge.node.frontmatter.tags.toLowerCase().includes(searchString) 
+			edge.node.frontmatter.title = edge.node.frontmatter.title.replace(/(<mark className="yellow">|<\/mark>)/gim, '');
+			edge.node.frontmatter.tags = edge.node.frontmatter.tags.replace(/(<mark classname="yellow">|<\/mark>)/gim, '');
+
+			if (edge.node.frontmatter.title.toLowerCase().includes(searchString) ||
+			edge.node.frontmatter.tags.toLowerCase().includes(searchString)){
+
+				const regex = new RegExp(searchString, 'gi');
+				edge.node.frontmatter.title = edge.node.frontmatter.title.replace(regex, '<mark className="yellow">$&</mark>');
+				edge.node.frontmatter.tags = edge.node.frontmatter.tags.replace(regex, '<mark className="yellow">$&</mark>');
+				return true
+			}
+			return false
 		})
 		setAllMarkdownRemark(filteredData);
 	}
@@ -20,11 +30,11 @@ const IndexPage = ({data}) => {
             </div>
 			{allMarkdownRemark.map((edge)=>(
 				<div key = {edge.node.id} className="card-panel hoverable">
-					<h3>{edge.node.frontmatter.title }</h3>
+					<h3 dangerouslySetInnerHTML={{ __html: edge.node.frontmatter.title }} /> 
 					<p>by <b>{edge.node.frontmatter.author}</b><br/>{edge.node.frontmatter.date }</p>
 					<p>{edge.node.excerpt }</p>
 					{edge.node.frontmatter.tags.split(",").map((tag)=>(
-						<div key={tag} className="chip">{tag}</div>
+						<div key={tag} className="chip" dangerouslySetInnerHTML={{__html:tag}}/>
 					)) }
 					<br/><br/>
 					<Link to ={edge.node.frontmatter.path }>Read More</Link>
